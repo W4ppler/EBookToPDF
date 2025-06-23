f0e474fc255fc1ebbd5420a789005dc478a48c64
manue <manuel.hofmarcher27@gmail.com>
2025-06-23 17:23:47 +0200

    added timestamps to the print commands. Script now successfully saves all pages as images, crops them and converts them to a pdf.

--- EBookToPDF.py
+++ EBookToPDF.py
@@ -1,7 +1,26 @@
+import time
 from time import sleep
+
+from selenium.webdriver import Keys
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
 from selenium import webdriver
 from selenium.common import NoSuchElementException
-from selenium.webdriver.common.by import By
+from PIL import Image
+from datetime import datetime
+
+
+""""
+TODO:
+variable sleeps times (alle 100ms check of geladen)
+eta / buch (progress bar)
+dir für bücher
+delete pics after conversion
+
+"""
+
+
 
 def login(browser):
     email_field = browser.find_element(By.ID, "ion-input-0")
@@ -10,8 +29,8 @@ def login(browser):
     # email = input("email: ")
     # passwd = input("password: ")
 
-    email = "adadadasddas"
-    passwd = "dsfvsdfsdf"
+    email = "awdawdawdawdawdawd@:(.:)"
+    passwd = "Password1234567890"
 
     email_field.clear()
     email_field.send_keys(email)
@@ -25,7 +44,7 @@ def login(browser):
     sleep(2)
 
     if check_button_existence(By.ID, "ion-input-0", browser):
-        print("Login failed")
+        print(datetime.now().strftime("%H:%M:%S") + " Login failed")
         browser.find_element(By.CLASS_NAME, "alert-button").click()
         login(browser)
 
@@ -39,7 +58,7 @@ def check_button_existence(locator_type, locator_value, browser):
 
 
 def get_books(browser):
-    # browser.execute_script("document.body.style.zoom='10%'") # Zooms very far out to render all books
+    browser.execute_script("document.body.style.zoom='10%'") # Zooms very far out to render all books
     book_names = browser.find_elements(By.CLASS_NAME, "entry-heading")
 
     books = list()
@@ -54,51 +73,76 @@ def book_selection(browser, books):
     for book in books:
         print("[" + str(books.index(book)) + "] " + book)
 
-    selected_book = input("Select a book: ")
+    selected_book = input(datetime.now().strftime("%H:%M:%S") + " Select a book: ")
 
     try:
         if books[int(selected_book)]:
             selected_book = books[int(selected_book)]
     except (ValueError, IndexError):
-        print("Book not found")
+        print(datetime.now().strftime("%H:%M:%S") + " Book not found")
         return -1
 
     browser.find_element(By.XPATH, f"//h2[contains(text(), '{selected_book}')]").click()
     sleep(2)
     browser.switch_to.window(browser.window_handles[-1])
 
-    print(f"Selected book: {selected_book}")
+    print(datetime.now().strftime("%H:%M:%S") + f" Selected book: {selected_book}")
 
     return 0
 
 
 def save_book_as_pdf(browser):
-    div_id = "pg1Overlay"
+    browser.execute_script("document.body.style.zoom = 2.5") # with 2.5 zoom, the page is filling the whole viewport
 
-    # calcs the needed zoom factor to fit the page in the window
-    zoom_script = f"""
-    var element = document.getElementById('{div_id}');
-    var rect = element.getBoundingClientRect();
-    var w = window.innerWidth;
-    var h = window.innerHeight;
-    var zx = w / rect.width;
-    var zy = h / rect.height;
-    var z = Math.min(zx, zy);
-    document.body.style.zoom = z;
-    return z;
-    """
+    page = 1
+    page_list = list()
+    page_text_field = browser.find_element(By.ID, "txtPage")
 
 
-    # browser.execute_script(zoom_script)
+    # initially sets the page to 1
+    page_text_field.send_keys(page)
+    page_text_field.send_keys(Keys.ENTER)
 
-    sleep(2)
+    sleep(3)
 
-    browser.save_full_page_screenshot("fullpage.png")
+    page_list = []
+    while not check_button_existence(By.CLASS_NAME, "ui-dialog", browser):
+        browser.save_screenshot(f"{page}.png")
+        print(datetime.now().strftime("%H:%M:%S") + " Saving page " + str(page))
+
+        crop_image(f"{page}.png")
+        print(datetime.now().strftime("%H:%M:%S") + " Cropped page " + str(page))
+
+        page_list.append(f"{page}.png")
+        page += 1
+        page_text_field.send_keys(page)
+        page_text_field.send_keys(Keys.ENTER)
+        sleep(3)
+
+    print(datetime.now().strftime("%H:%M:%S") + " Finished saving pages, now creating PDF...")
+
+    images = []
+    for f in page_list:
+        images.append(Image.open(f).convert("RGB"))
+
+    images[0].save("output.pdf", save_all=True, append_images=images[1:], resolution=300.0)
+
+    print(datetime.now().strftime("%H:%M:%S") + " PDF created successfully.")
+
+
+def crop_image(image):
+    with Image.open(image) as img:
+        cropped = img.crop((96, 128, 2368, 3343))
+        cropped.save(f"{image}")
+
 
 def main():
-    with webdriver.Firefox() as browser:
+    options = Options()
+    options.add_argument('--headless')
+    options.add_argument(f'--window-size=2480,3508')
+
+    with webdriver.Chrome(options=options) as browser:
         browser.get("https://digi4school.at/ebooks")
-        browser.set_window_size(5000, 5000)
 
         sleep(2)
 
@@ -110,7 +154,7 @@ def main():
         books = get_books(browser)
 
         while book_selection(browser, books) != 0:
-            print("Please select a valid book.")
+            print(datetime.now().strftime("%H:%M:%S") + " Please select a valid book.")
 
         sleep(5)
 
@@ -118,9 +162,6 @@ def main():
 
 
 
+
 if __name__ == '__main__':
     main()
-
-"""
-browser.save_full_page_screenshot("imageToSave.png")
-"""
