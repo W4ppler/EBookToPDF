8e3780d33f90bf4e35bb458eab5277b74e26f303
manue <manuel.hofmarcher27@gmail.com>
2025-06-28 17:04:50 +0200

    normalized sleep times moved exports to their own directory started the "all" option in the book selection (not finished) created subbok selection (some ebooks contain subdirectories for ressources, audios and the real ebook) fixed the mechanism of how the window is zoomed and the pics are cropped changed the way of how the save_book_as_pdf function notices the end of the book

--- EBookToPDF.py
+++ EBookToPDF.py
@@ -1,29 +1,28 @@
 import os
-import time
+from logging import setLoggerClass
 from time import sleep
 
-from selenium.webdriver import Keys
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium import webdriver
-from selenium.common import NoSuchElementException
+from selenium.common import NoSuchElementException, ElementNotInteractableException
 from PIL import Image
 from datetime import datetime
-
+import numpy
 
 """"
 TODO:
-x variable sleeps times (alle 100ms check of geladen)
-eta / buch (progress bar)
-dir für bücher
-x delete pics after conversion
-OCR 
 compatible with all books?
 english books
+    timeout for materials and audios
+infotour
+next page popup on each page
+convert all books
 """
 
 loading_time_between_pages = 1
+bookname = "book.pdf"
+
 
 def login(browser):
     email_field = browser.find_element(By.ID, "ion-input-0")
@@ -32,8 +31,8 @@ def login(browser):
     # email = input("email: ")
     # passwd = input("password: ")
 
-    email = "sdfsdfsdf"
-    passwd = "dfdfgdf"
+    email = "sdrgrgddrgdrsef"
+    passwd = "sdrgdrgdgrsef"
 
     email_field.clear()
     email_field.send_keys(email)
@@ -43,7 +42,8 @@ def login(browser):
 
     shadow_host = browser.find_element(By.CSS_SELECTOR, "ion-button[type='submit']")
     browser.execute_script("arguments[0].shadowRoot.querySelector('button[type=\"submit\"]').click();", shadow_host)
-    sleep(2)
+
+    sleep(loading_time_between_pages)
 
     if check_button_existence(By.ID, "ion-input-0", browser):
         print(datetime.now().strftime("%H:%M:%S") + " Login failed")
@@ -60,75 +60,129 @@ def check_button_existence(locator_type, locator_value, browser):
 
 
 def get_books(browser):
-    browser.execute_script("document.body.style.zoom='10%'") # Zooms very far out to render all books
-    book_names = browser.find_elements(By.CLASS_NAME, "entry-heading")
+    browser.execute_script("document.body.style.zoom='10%'")  # Zooms very far out to render all books
+    books = browser.find_elements(By.CLASS_NAME, "entry-heading")
 
-    books = list()
-    for book in book_names:
-        books.append(book.text)
+    book_names = list()
+    for book in books:
+        book_names.append(book.text)
 
-    browser.execute_script("document.body.style.zoom='100%'")
-    return books
+    return book_names
 
 
 def book_selection(browser, books):
+    global bookname
+
     for book in books:
         print("[" + str(books.index(book)) + "] " + book)
 
     selected_book = input(datetime.now().strftime("%H:%M:%S") + " Select a book: ")
 
+    if selected_book.lower() == 'all':
+        pass
+
     try:
-        if books[int(selected_book)]:
-            selected_book = books[int(selected_book)]
+        selected_book = books[int(selected_book)]
     except (ValueError, IndexError):
         print(datetime.now().strftime("%H:%M:%S") + " Book not found")
         return -1
 
-    browser.find_element(By.XPATH, f"//h2[contains(text(), '{selected_book}')]").click()
-    sleep(2)
+    browser.find_element(By.XPATH, f'//h2[contains(text(), "{selected_book}")]').click()
+    bookname = selected_book.replace("/", "_")
+    sleep(loading_time_between_pages)
     browser.switch_to.window(browser.window_handles[-1])
 
     print(datetime.now().strftime("%H:%M:%S") + f" Selected book: {selected_book}")
+    return 0
+
+
+
+def subbook_selection(browser):
+    global bookname
+    book_elements = browser.find_elements(By.CLASS_NAME, "tx")
+    books = list()
+
+    for book_element in book_elements:
+        if book_element.text:
+            books.append(book_element.text)
+
+    for book in books:
+        print("[" + str(books.index(book)) + "] " + book)
+
+    selected_book = input(datetime.now().strftime("%H:%M:%S") + " Select a subbook: ")
+
+    try:
+        selected_book = books[int(selected_book)]
+    except (ValueError, IndexError):
+        print(datetime.now().strftime("%H:%M:%S") + " This subbook does not exist")
+        return -1
+
+    browser.find_element(By.XPATH, f"//h1[contains(text(), '{selected_book}')]").click()
+    bookname = os.path.join(bookname, selected_book.replace("/", "_"))
+    sleep(loading_time_between_pages)
+    browser.switch_to.window(browser.window_handles[-1])
+
+    print(datetime.now().strftime("%H:%M:%S") + f" Selected subbook: {selected_book}")
 
     return 0
 
 
 def save_book_as_pdf(browser):
+    global bookname
     page = 1
 
-    while not check_button_existence(By.ID, "txtPage", browser):
-        sleep(0.1)
-        print(datetime.now().strftime("%H:%M:%S") + " Waiting for book to load...")
-
     if check_button_existence(By.ID, "routlineClose", browser):
-        browser.find_element(By.ID, "routlineClose").click()
-        print(datetime.now().strftime("%H:%M:%S") + " Closed outline popup")
+        print(datetime.now().strftime("%H:%M:%S") + " Detected outline popup")
+        try:
+            browser.find_element(By.ID, "routlineClose").click()
+            print(datetime.now().strftime("%H:%M:%S") + " Closed outline popup")
+        except ElementNotInteractableException:
+            print(datetime.now().strftime("%H:%M:%S") + " Outline popup isnt visible")
 
     browser.find_element(By.ID, "btnFirst").click()
 
-    while not check_button_existence(By.ID, "txtPage", browser):
-        sleep(0.1)
-        print(datetime.now().strftime("%H:%M:%S") + " Waiting for book to load...")
+    sleep(loading_time_between_pages)
 
-    sleep(3)  # first page might take a bit longer to load
+    if check_button_existence(By.ID, "btnZoomHeight", browser):
+        browser.find_element(By.ID, "btnZoomHeight").click()
+    else:
+        browser.execute_script(f"document.body.style.zoom = 2")
 
-    # zoom = calculate_zoom(browser)
-    browser.execute_script(f"document.body.style.zoom = 3")
 
-    page_list = []
-    # while not check_button_existence(By.CLASS_NAME, "ui-dialog", browser):
+    div_rect = browser.execute_script(f"""
+        var el = document.getElementById('pg1Overlay');
+        var rect = el.getBoundingClientRect();
+        return rect;
+    """)
+
+    if not os.path.exists(bookname):
+        os.makedirs(bookname)
+        print(datetime.now().strftime("%H:%M:%S") + f" Created directory for book: {bookname}")
+
     i = 0
-    while i < 5:
-        browser.save_screenshot(f"{page}.png")
+    page_list = []
+    while i < 5: # only for testing
 
-        # crop_image(f"{page}.png")
+    # while True:
+        browser.save_screenshot(f"{bookname}" + "/" + f"{page}.png")
+
+        crop_image(f"{bookname}" + "/" + f"{page}.png", div_rect)
         print(datetime.now().strftime("%H:%M:%S") + " Saved and cropped page " + str(page))
 
-        page_list.append(f"{page}.png")
-        page += 1
+        page_list.append(f"{bookname}" + "/" + f"{page}.png")
+
+        if len(page_list) > 1:
+            if images_are_identical(page_list[-2], page_list[-1]):
+                os.remove(page_list[-1])
+                page_list.pop()
+                break
+
         browser.find_element(By.ID, "btnNext").click()
-        sleep(loading_time_between_pages)
+
+        sleep(loading_time_between_pages/2)
         i += 1
+
+        page += 1
 
 
     print(datetime.now().strftime("%H:%M:%S") + " Finished saving pages, now creating PDF...")
@@ -137,7 +191,8 @@ def save_book_as_pdf(browser):
     for f in page_list:
         images.append(Image.open(f).convert("RGB"))
 
-    images[0].save("output.pdf", save_all=True, append_images=images[1:], resolution=300.0)
+    pdf_filename = os.path.basename(bookname)
+    images[0].save(f"{bookname}/{pdf_filename}.pdf", save_all=True, append_images=images[1:], resolution=300.0)
 
     print(datetime.now().strftime("%H:%M:%S") + " PDF created successfully.")
 
@@ -145,40 +200,36 @@ def save_book_as_pdf(browser):
         os.remove(i)
 
 
-def calculate_zoom(browser):
-    overlay_rect = browser.execute_script("""
-        var el = document.getElementById('pg1Overlay');
-        var rect = el.getBoundingClientRect();
-        return {width: rect.width, height: rect.height};
-    """)
-
-
-    viewport_width = browser.execute_script("return window.innerWidth;")
-    viewport_height = browser.execute_script("return window.innerHeight;")
-
-    available_height = viewport_height - 40 # navbar
-
-    zoom_w = viewport_width / overlay_rect['width']
-    zoom_h = available_height / overlay_rect['height']
-
-    return min(zoom_w, zoom_h)
-
-
-def crop_image(image):
+def crop_image(image, div_rect):
     with Image.open(image) as img:
-        cropped = img.crop((96, 128, 2368, 3343))
+        cropped = img.crop((div_rect["left"] + 1, div_rect["top"] + 1, div_rect["right"], div_rect["bottom"]))
         cropped.save(f"{image}")
+
+
+
+
+def images_are_identical(path1, path2):
+    if not os.path.exists(path1) or not os.path.exists(path2):
+        return False
+
+    with Image.open(path1) as img1, Image.open(path2) as img2:
+        if img1.size != img2.size:
+            return False
+        arr1 = numpy.array(img1)
+        arr2 = numpy.array(img2)
+        return numpy.array_equal(arr1, arr2)
 
 
 def main():
     global loading_time_between_pages
     options = Options()
     options.add_argument('--headless')
-    options.add_argument(f'--window-size=3720,5262') # 2480,3508
+    options.add_argument(f'--window-size=3000,3608')  # 2480,3508
 
     with webdriver.Chrome(options=options) as browser:
         while True:
-            user_input = input("Enter loading time between pages (in seconds, default is 1, slower internet - higher, faster internet - smaller): ")
+            user_input = input(
+                "Enter loading time between pages (in seconds, default is 1, slower internet - higher, faster internet - smaller): ")
             if user_input == "":
                 loading_time_between_pages = 1
                 break
@@ -190,23 +241,25 @@ def main():
 
         browser.get("https://digi4school.at/ebooks")
 
-        while not check_button_existence(By.TAG_NAME, "img", browser):
-            sleep(0.1)
-            print(datetime.now().strftime("%H:%M:%S") + " Waiting for page to load...")
+        sleep(loading_time_between_pages)
 
         if check_button_existence(By.ID, "ion-input-0", browser):
             login(browser)
 
-        while not check_button_existence(By.TAG_NAME, "app-book-list-entry", browser):
-            sleep(0.1)
-            print(datetime.now().strftime("%H:%M:%S") + " Waiting for books to load...")
+        sleep(loading_time_between_pages)
 
         books = get_books(browser)
 
         while book_selection(browser, books) != 0:
             print(datetime.now().strftime("%H:%M:%S") + " Please select a valid book.")
 
+        sleep(loading_time_between_pages)
+
+        if check_button_existence(By.CLASS_NAME, "tx", browser):
+            subbook_selection(browser)
+
         save_book_as_pdf(browser)
+
 
 if __name__ == '__main__':
     main()
